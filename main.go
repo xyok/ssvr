82a5b757db49a8c6bb1ec2476b3dacdd145bdfa9
xyok <xy___ok@163.com>
2019-01-01 22:27:34 +0800

    init

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"path/filepath"
+)
+
+func main() {
+	port := flag.String("port", "9999", "http port")
+	dir := flag.String("dir", "./", "http port")
+	flag.Parse()
+	absPath, err := filepath.Abs(*dir)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("start server port:" + *port + " path :" + absPath)
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(*dir))))
+	err = http.ListenAndServe(":"+*port, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
